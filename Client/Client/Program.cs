af5a7f951842e4258667e32207ec64f12cd58c8d
NamChuMinh <chuminhnamma@gmail.com>
2022-11-02 15:49:56 +0700

    first commit

---
+++ Client/Client/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Bước 1: Lấy địa chỉ IpEndPoint của server UDP cần trao đổi tin
+                IPEndPoint s_iep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+                //Bước 2: Tạo socket UDP 
+                Socket clientSocket = new Socket(SocketType.Dgram, ProtocolType.Udp);
+                //Bước 3: Trao đổi tin (gửi/nhận)
+
+                while (true)
+                {
+                    //Gửi tin lên server UDP
+                    Console.Write("Nhap chuoi can gui len Sever: \n");
+                    string messageSend = Console.ReadLine();
+                    byte[] dataSend = ASCIIEncoding.ASCII.GetBytes(messageSend);
+                    clientSocket.SendTo(dataSend, s_iep);//Truyền tin data tới địa chỉ s_iep
+
+                    //Nhận tin từ server trả về
+                    byte[] dataReceive = new byte[1024];
+                    EndPoint iep = new IPEndPoint(IPAddress.None,0);//Tạo iep rỗng với port 0
+                    clientSocket.ReceiveFrom(dataReceive, ref iep);
+                    string messageReceive = ASCIIEncoding.ASCII.GetString(dataReceive);
+                    Console.Write("<server>: " + messageReceive);
+
+                    if(messageSend.ToLower() == "thoat")
+                    {
+                        //Bước 4: Đóng kết nối
+                        clientSocket.Close();
+                        return;
+                    }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lỗi: " + ex.Message);
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Bước 1: Tạo IpEndPoint server UDP
+                IPEndPoint s_iep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+                //Bước 2: Tạo socket UDP
+                Socket serverSocket = new Socket(SocketType.Dgram, ProtocolType.Udp);
+                //Bước 3: Đăng ký địa chỉ s_iep cho server socket UDP
+                serverSocket.Bind(s_iep);
+                //Bước 4: Gửi nhận gói tin
+
+                while (true)
+                {
+                    //Nhận tin được gửi đến từ client
+                    byte[] dataReceive = new byte[1024];
+                    //Lấy địa chỉ EndPoint của client để thực hiện gửi lại tin cho client đó
+                    EndPoint c_iep = new IPEndPoint(IPAddress.None, 0); //Ban đầu địa chỉ EndPoint của client là rỗng và port là 0
+                    serverSocket.ReceiveFrom(dataReceive, ref c_iep);//Lấy data của clinet gửi lên và gán vào dataReceive và c_iep
+                    string messageReceive = ASCIIEncoding.ASCII.GetString(dataReceive);
+                    Console.Write("<client>: " + messageReceive);
+
+                    //Gửi lại tin cho client
+                    Console.Write("Nhap chuoi can gui ve Client: \n");
+                    string messageSend = Console.ReadLine();
+                    byte[] dataSend = ASCIIEncoding.ASCII.GetBytes(messageSend);
+                    serverSocket.SendTo(dataSend, c_iep);
+
+                    if (messageReceive.ToLower() == "thoat")
+                    {
+                        //Bước 5: Đóng kết nối
+                        serverSocket.Close();
+                        return;
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Lỗi: " + ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
